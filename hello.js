0f3b5be5da70e392bb56971fceaef77df6a556e9
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-11 18:20:44 +0200

    for loop in node

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
-for(var i = 0; i < 10; i++) {
-    console.log('hello from js.');
+// for(var i = 0; i < 10; i++) {
+//     console.log('hello from js.');
+// }
+function echo(str, num) {
+    for(var i = 0; i <= num; i++) {
+        console.log(str, num)
+    }
+    
 }
+echo('echo!!!', 10)
+echo('Tater Tots', 3)
