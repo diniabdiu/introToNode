7817b671c6898b2e6c15358f799ae0fb42930f52
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-11 18:30:06 +0200

    find average

--- hello.js
+++ hello.js
@@ -1,11 +1,29 @@
 // for(var i = 0; i < 10; i++) {
 //     console.log('hello from js.');
 // }
-function echo(str, num) {
-    for(var i = 0; i <= num; i++) {
-        console.log(str, num)
-    }
+// function echo(str, num) {
+//     for(var i = 0; i <= num; i++) {
+//         console.log(str, num)
+//     }
     
+// }
+// echo('echo!!!', 10)
+// echo('Tater Tots', 3)
+function average(scores) {
+    // Add all scores together
+    var total = 0;
+    scores.forEach(function(score) {
+        total += score;
+    });
+    // Divide by total number of scores
+    var avg = total/scores.length
+    // Round average
+    return Math.round(avg);
+   
+
 }
-echo('echo!!!', 10)
-echo('Tater Tots', 3)
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores)); // Should return 94
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log(average(scores2)); // Should return 68
