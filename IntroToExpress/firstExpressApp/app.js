d80e1e1d45a5c8482d520d090da89f2f03944501
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-12 15:07:47 +0200

    route Params

--- IntroToExpress/firstExpressApp/app.js
+++ IntroToExpress/firstExpressApp/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function(req, res) {
     res.send('Hi there!');
 });
@@ -10,6 +11,29 @@ app.get('/bye', function(req, res){
 app.get('/dog', function(req, res){ 
     res.send('meow');
 });
+
+app.get('/r/:subredditName', function(req, res) {
+    var subreddit = req.params.subredditName;
+
+    console.log(req.params);
+    res.send('WELCOME TO THE ' + subreddit.toUpperCase() + ' SUBREDDIT');
+});
+
+// app.get('/r/subredditname');
+app.get('/r/:subredditName/comments/:id/:title/', function(req, res) {
+    res.send('WELCOME TO THE COMMENTS PAGE!')
+});
+
+app.get('/r/music');
+app.get('/')
+
+app.get('*', function(req, res) {
+    // res.send(`<a href="localhost:3050">HIT THE LINK</a>`);
+    // res.render('Hit the link below, your url is wrong');
+    res.send(`<a href="/">Hit me!</a>`);
+    // res.end();
+});
+
 app.listen(3050, process.env.IP, function() {
     console.log('serever has started!');
 });
