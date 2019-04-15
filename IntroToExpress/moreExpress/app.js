bd8e7916d18508b7f051ba931aa4e8c04ae4d6b1
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-15 13:13:00 +0200

    template ejs

--- IntroToExpress/moreExpress/app.js
+++ IntroToExpress/moreExpress/app.js
@@ -5,6 +5,12 @@ app.get('/', function(req, res) {
     res.render('home.ejs');
 });
 
+app.get('/fallinlovewith/:thing', function(req, res) {
+    var thing = req.params.thing;
+    res.render('love.ejs', {thingVAR: thing});
+    // res.send('You fell in love with ' + thing);
+});
+
 app.listen(3001, process.env.IP, function() {
     console.log('Server is Listening!!!');
 });
