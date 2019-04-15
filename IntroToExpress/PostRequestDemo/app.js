ed36b9499e054d4ef24e8acc69b704e243b9af20
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-15 16:55:02 +0200

    post+get method

--- IntroToExpress/PostRequestDemo/app.js
+++ IntroToExpress/PostRequestDemo/app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+var friends = ['Tony', 'Miranda', 'justin', 'Pierre', 'Lily'];
 
 app.get('/', function(req, res) {
     res.render('home');
 });
 app.post('/addfriend', function(req, res) {
-    res.send('YOU HAVE REACHED THE POST ROUTE'); 
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect('/friends'); 
+
 });
 app.get('/friends', function(req, res) {
-    var friends = ['Tony', 'Miranda', 'justin', 'Pierre', 'Lily'];
-
     res.render('friends', {friends: friends});
 });
 app.listen(3010, process.env.IP, function() {
