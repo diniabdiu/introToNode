2564e274ba4a9cdb35221a28c803def0cf747ed4
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-15 13:44:51 +0200

    ejs Loops

--- IntroToExpress/moreExpress/app.js
+++ IntroToExpress/moreExpress/app.js
@@ -10,6 +10,15 @@ app.get('/fallinlovewith/:thing', function(req, res) {
     res.render('love.ejs', {thingVAR: thing});
     // res.send('You fell in love with ' + thing);
 });
+app.get('/posts', function(req, res) {
+    var posti = [
+        {title: 'Post 1', author: 'Susy'},
+        {title: 'My adorable pet bunny', author: 'Charlie'},
+        {title: 'Can you believe this pomsky?', author: 'Dini'}
+    ];
+
+    res.render('posts.ejs', {posts: posti});
+});
 
 app.listen(3001, process.env.IP, function() {
     console.log('Server is Listening!!!');
